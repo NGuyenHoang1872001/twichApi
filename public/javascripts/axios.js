622649b658e542ab10adc9dac580541d3250ed6c
hoang <nguyenhoang1872011@gmail.com>
2022-01-06 19:55:37 +0700

    first commit

--- public/javascripts/axios.js
+++ public/javascripts/axios.js
@@ -1,43 +1,55 @@
-
 const fetchChannels = async (type) => {
   try {
-    const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    const channels = [
+      "ESL_SC2",
+      "OgamingSC2",
+      "cretetion",
+      "freecodecamp",
+      "storbeck",
+      "habathcx",
+      "RobotCaleb",
+      "noobs2ninjas",
+    ];
     let dataset = [];
 
     const promises = channels.map(async (channel) => {
-      const { data } = await axios.get(`https://twitch-proxy.freecodecamp.rocks/twitch-api/${type}/${channel}`);
+      const { data } = await axios.get(
+        `https://twitch-proxy.freecodecamp.rocks/twitch-api/${type}/${channel}`
+      );
       return data;
     });
 
     dataset = await Promise.all(promises);
     return dataset;
-
   } catch (error) {
-    console.log("🚀 ~ file: axios.js ~ line 16 ~ fetchAllStreamers ~ error", error)   
+    console.log(
+      "🚀 ~ file: axios.js ~ line 16 ~ fetchAllStreamers ~ error",
+      error
+    );
   }
-}
+};
 
 const renderOnlineChannels = (streamers, onlineChannels) => {
-  onlineChannels.forEach(item => { 
-    if(item.stream == null) {
+  onlineChannels.forEach((item) => {
+    if (item.stream == null) {
       return;
     }
 
     // Render streamer list
-    const streamerItem = document.createElement('li');
-    streamerItem.classList.add('online');
+    const streamerItem = document.createElement("li");
+    streamerItem.classList.add("online");
 
     // Render streamer's logo
-    const imgEle = document.createElement('img'); // src
+    const imgEle = document.createElement("img"); // src
     imgEle.src = item.stream.channel.logo;
 
     // Render streamer's link
-    const linkEle = document.createElement('a'); // href
+    const linkEle = document.createElement("a"); // href
     linkEle.innerHTML = item.stream.channel.display_name;
     linkEle.href = item.stream.channel.url;
 
     // Render streamer's status
-    const statusEle = document.createElement('span');
+    const statusEle = document.createElement("span");
     statusEle.innerHTML = `${item.stream.game}: ${item.stream.channel.status}`;
 
     streamerItem.appendChild(imgEle);
@@ -45,26 +57,30 @@ const renderOnlineChannels = (streamers, onlineChannels) => {
     streamerItem.appendChild(statusEle);
 
     streamers.appendChild(streamerItem);
-  })
-}
+  });
+};
 
 const renderOfflineChannels = (streamers, offlineChannels) => {
-  offlineChannels.forEach(item => { 
+  console.log(
+    "🚀 ~ file: axios.js ~ line 64 ~ renderOfflineChannels ~ offlineChannels",
+    offlineChannels
+  );
+  offlineChannels.forEach((item) => {
     // Render streamer list
-    const streamerItem = document.createElement('li');
-    streamerItem.classList.add('offline');
+    const streamerItem = document.createElement("li");
+    streamerItem.classList.add("offline");
 
     // Render streamer's logo
-    const imgEle = document.createElement('img'); // src
+    const imgEle = document.createElement("img"); // src
     imgEle.src = item.logo;
 
     // Render streamer's link
-    const linkEle = document.createElement('a'); // href
+    const linkEle = document.createElement("a"); // href
     linkEle.innerHTML = item.display_name;
     linkEle.href = item.url;
 
     // Render streamer's status
-    const statusEle = document.createElement('span');
+    const statusEle = document.createElement("span");
     statusEle.innerHTML = `Offline`;
 
     streamerItem.appendChild(imgEle);
@@ -72,45 +88,73 @@ const renderOfflineChannels = (streamers, offlineChannels) => {
     streamerItem.appendChild(statusEle);
 
     streamers.appendChild(streamerItem);
-  })
-}
+  });
+};
 
 // BOM
-window.addEventListener('load', async () => {
+window.addEventListener("load", async () => {
   // Fetch all channels
-  const channels = await fetchChannels('channels');
+  const channels = await fetchChannels("channels");
 
-  // Fetch online channels 
+  // Fetch online channels
   // HOF higher order function (map, filter, reduce, foreach || some, every, find, ... )
-  const onlineChannels = await (await fetchChannels('streams')).filter(channel => channel.stream !== null);
+  const onlineChannels = await (
+    await fetchChannels("streams")
+  ).filter((channel) => channel.stream !== null);
 
-  // Get online channels's name 
-  const onlineChannelsName = onlineChannels.map(item => item.stream.channel.display_name );
-  console.log("🚀 ~ file: axios.js ~ line 25 ~ window.addEventListener ~ onlineChannelsName", onlineChannelsName);
+  // Get online channels's name
+  const onlineChannelsName = onlineChannels.map(
+    (item) => item.stream.channel.display_name
+  );
+  console.log(
+    "🚀 ~ file: axios.js ~ line 25 ~ window.addEventListener ~ onlineChannelsName",
+    onlineChannelsName
+  );
 
   // Get offline channels
-  const offlineChannels = channels.filter(channel => !onlineChannelsName.includes(channel.display_name));
-  
-  console.log("🚀 ~ file: axios.js ~ line 30 ~ offlineChannels ~ offlineChannels", offlineChannels);
-  console.log("🚀 ~ file: axios.js ~ line 24 ~ window.addEventListener ~ onlineChannels", onlineChannels);
-  console.log("🚀 ~ file: axios.js ~ line 23 ~ window.addEventListener ~ streamers", channels);
+  const offlineChannels = channels.filter(
+    (channel) => !onlineChannelsName.includes(channel.display_name)
+  );
 
-  const streamers = document.querySelector('#streamers');
-  const onlineBtn = document.querySelector('#onlineBtn');
+  console.log(
+    "🚀 ~ file: axios.js ~ line 30 ~ offlineChannels ~ offlineChannels",
+    offlineChannels
+  );
+  console.log(
+    "🚀 ~ file: axios.js ~ line 24 ~ window.addEventListener ~ onlineChannels",
+    onlineChannels
+  );
+  console.log(
+    "🚀 ~ file: axios.js ~ line 23 ~ window.addEventListener ~ streamers",
+    channels
+  );
+
+  const streamers = document.querySelector("#streamers");
+  const onlineBtn = document.querySelector("#onlineBtn");
+  const offlineBtn = document.querySelector("#offlineBtn");
+  const allBtn = document.querySelector("#allBtn");
 
   renderOnlineChannels(streamers, onlineChannels);
   renderOfflineChannels(streamers, offlineChannels);
 
-  onlineBtn.addEventListener('click', () => {
+  onlineBtn.addEventListener("click", () => {
     while (streamers.firstChild) {
       streamers.removeChild(streamers.lastChild);
     }
-    renderOnlineChannels(streamers, onlineChannels)
-  })
+    renderOnlineChannels(streamers, onlineChannels);
+  });
+  offlineBtn.addEventListener("click", () => {
+    while (streamers.firstChild) {
+      streamers.removeChild(streamers.lastChild);
+    }
+    renderOfflineChannels(streamers, offlineChannels);
+  });
 
-  
-
-})
-
-
-
+  allBtn.addEventListener("click", () => {
+    while (streamers.firstChild) {
+      streamers.removeChild(streamers.lastChild);
+    }
+    renderOnlineChannels(streamers, onlineChannels);
+    renderOfflineChannels(streamers, offlineChannels);
+  });
+});
